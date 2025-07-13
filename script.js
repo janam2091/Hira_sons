0b4291ede20e82677ceceea58560a94ce3fdba53
AshmitSrivastava <ashmitsrivastava1607@gmail.com>
2025-07-13 16:59:42 +0530

    rjc arrow testing

--- script.js
+++ script.js
@@ -404,3 +404,24 @@ document.addEventListener('DOMContentLoaded', function() {
         updateOnScroll();
     }, 1500);
 });
+
+//Security 
+// document.onkeydown = (e) => {
+//     if (e.key == 123) {
+//         e.preventDefault();
+//         alert("Cant click")
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+//         e.preventDefault();
+//         alert("Cant click")
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+//         e.preventDefault();
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+//         e.preventDefault();
+//     }
+//     if (e.ctrlKey && e.key == 'U') {
+//         e.preventDefault();
+//     }
+// };
